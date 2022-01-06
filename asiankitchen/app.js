57bfb4f98f94c25fe5ba81e6bc778ab388b16f76
serkancagman <serkancagmann@gmail.com>
2022-01-06 19:57:09 +0300

    update

--- asiankitchen/app.js
+++ asiankitchen/app.js
@@ -1,7 +1,7 @@
 const menu = [
   {
     id: 1,
-    title: "Tteokbokki",
+    title: "Tteokbokki2",
     category: "Korea",
     price: 10.99,
     img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
